ff89951054e466ecec93e852e67df3def77fa6b8
Deray <33969267+LOoLzeC@users.noreply.github.com>
2019-02-12 07:24:03 +0700

    Update index.php

--- geo/index.php
+++ geo/index.php
@@ -1,1 +1,27 @@
 
+
+<html>
+<head>
+	<title>hello</title>
+</head>
+<body>
+<script>alert('welcome');</script>
+<?php
+	$LOG=array(
+	"ip" => $_SERVER["HTTP_X_FORWARDED_FOR"],
+	"pr" => $_SERVER["REMOTE_PORT"],
+	"ua" => $_SERVER["HTTP_USER_AGENT"],
+	"rq" => $_SERVER["REQUEST_METHOD"]);
+	$js=json_encode($LOG);
+	$b=fopen("r.txt","a+");
+	$c=fopen("haha.txt","w");
+	fwrite($b,"$js
+");
+	fwrite($c,"$js
+");
+	fclose($b);
+?>
+<h3>your ip has been genjot
+</body>
+</html>
+	
